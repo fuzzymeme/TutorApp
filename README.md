87b47e576a89f51d2682c205ab585530422390c8
fuzzymeme <fuzzymeme@gmail.com>
2017-05-23 20:25:21 +0100

    Initial README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tutor App
+
+Mostly here to revisit Swift. 
---
+++ Tutor/QandAGroup.swift
@@ -0,0 +1,9 @@
+//
+//  QandAGroup.swift
+//  Tutor
+//
+//  Created by Richard Smith on 23/05/2017.
+//  Copyright © 2017 Richard Smith. All rights reserved.
+//
+
+import Foundation
