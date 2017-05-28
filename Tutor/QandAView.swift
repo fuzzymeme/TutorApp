28dd99dea09ee8e7b904ffaeae70d5f1e2192ed8
fuzzymeme <fuzzymeme@gmail.com>
2017-05-28 22:55:16 +0100

    Updated QandAView to have buttons

--- Tutor/QandAView.swift
+++ Tutor/QandAView.swift
@@ -2,7 +2,7 @@
 //  QandAView.swift
 //  Tutor
 //
-//  Created by Richard Smith on 28/05/2017.
+//  Created by Fuzzymeme on 28/05/2017.
 //  Copyright © 2017 Fuzzymeme. All rights reserved.
 //
 
@@ -10,11 +10,37 @@ import UIKit
 
 class QandAView: UIView {
     
+    @IBOutlet weak var optionOneButton: UIButton!
+    @IBOutlet weak var optionTwoButton: UIButton!
+    @IBOutlet weak var optionThreeButton: UIButton!
+    @IBOutlet weak var optionFourButton: UIButton!
+    
+    var optionOne: String {
+        get { return optionOneButton.currentTitle ?? ""}
+        set { optionOneButton.setTitle(newValue, for: .normal)}
+    }
+
+    var optionTwo: String {
+        get { return optionTwoButton.currentTitle ?? ""}
+        set { optionTwoButton.setTitle(newValue, for: .normal)}
+    }
+
+    var optionThree: String {
+        get { return optionOneButton.currentTitle ?? ""}
+        set { optionThreeButton.setTitle(newValue, for: .normal)}
+    }
+
+    var optionFour: String {
+        get { return optionFourButton.currentTitle ?? ""}
+        set { optionFourButton.setTitle(newValue, for: .normal)}
+    }
 
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
     }
+    
+    
 
 }
