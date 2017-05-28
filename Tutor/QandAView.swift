9275691ff31e73a8840b314ec06c21996d026a3c
fuzzymeme <fuzzymeme@gmail.com>
2017-05-28 22:23:01 +0100

    Initial QandAView

---
+++ Tutor/QandAView.swift
@@ -0,0 +1,20 @@
+//
+//  QandAView.swift
+//  Tutor
+//
+//  Created by Richard Smith on 28/05/2017.
+//  Copyright © 2017 Fuzzymeme. All rights reserved.
+//
+
+import UIKit
+
+class QandAView: UIView {
+    
+
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+}
