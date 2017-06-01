2815c5feed9a14d46b226d015d71b08e6af8d98c
fuzzymeme <fuzzymeme@gmail.com>
2017-06-01 10:52:31 +0100

    Separated the Json Model Reader from the model class

---
+++ Tutor/JsonModelReader.swift
@@ -0,0 +1,98 @@
+//
+//  JsonModelReader.swift
+//  Tutor
+//
+//  Created by Fuzzymeme on 31/05/2017.
+//  Copyright © 2017 Fuzzymeme. All rights reserved.
+//
+
+import Foundation
+
+struct JsonModelReader {
+    
+    func retrieveFromJsonFile(filename: String) -> [Int: KnowledgeEntry] {
+        
+        let filePath = Bundle.main.resourcePath!
+        var entries = [Int: KnowledgeEntry]()
+        
+        //        let data = try! String(contentsOfFile: filePath + "/basic_vocabulary_en2de.json", encoding: String.Encoding.utf8)
+        let data = try! String(contentsOfFile: filePath + "/" + filename, encoding: String.Encoding.utf8)
+        
+        if let entriesDict = convertToDictionary(text: data) {
+            for(key, value) in entriesDict {
+                if let newEntry = parseEntry(key: key, value: value) {
+                    entries[newEntry.id] = newEntry
+                }
+            }
+        }
+        
+        for entry in entries {
+            print(entry)
+        }
+        
+        return entries
+        
+    }
+    
+    private func parseEntry(key: String, value: Any?) -> KnowledgeEntry? {
+        
+        var gapHistory = [Int]()
+        var wrongAnswers = [WrongAnswer]()
+        
+        if let entry = value as? [String: Any] {
+            if entry["gapHistory"] != nil,
+                let nonEmpty = entry["gapHistory"] as? [Int] {
+                gapHistory = nonEmpty
+            }
+            
+            if entry["wrongAnswers"] != nil,
+                let nonEmpty = entry["wrongAnswers"] as? [String : Any] {
+                wrongAnswers.append(contentsOf: parseWrongAnswers(wrongAnswerInput: nonEmpty))
+            }
+            
+            if let id = Int(key),
+                let english = entry["english"] as? String,
+                let german = entry["german"] as? String,
+                let nextTime = entry["nextQuestionTime"] as? Int {
+                let entry = KnowledgeEntry(id: id, english: english, german: german, nextQuestionTime: nextTime, gapHistory: gapHistory, wrongAnswers: wrongAnswers)
+                return entry
+            } else {
+                return nil
+            }
+        }
+        return nil
+    }
+    
+    private func parseWrongAnswers(wrongAnswerInput: [String: Any?]) -> [WrongAnswer] {
+        var wrongAnswers = [WrongAnswer]()
+        
+        for wrongAnswerValue in wrongAnswerInput.values {
+            if let wrongAnswerDict = wrongAnswerValue as? [String : Int],
+                let wrongAnswer = parseWrongAnswer(wrongAnswerDict) {
+                wrongAnswers.append(wrongAnswer)
+            }
+        }
+        return wrongAnswers
+    }
+    
+    private func parseWrongAnswer(_ wrongAnswerInput: [String : Int]) -> WrongAnswer? {
+        if let id = wrongAnswerInput["id"],
+            let when = wrongAnswerInput["when"],
+            let count = wrongAnswerInput["count"] {
+            return WrongAnswer(id: id, when: when, count: count)
+        }
+        return nil
+    }
+    
+    private func convertToDictionary(text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+
+}
---
+++ TutorTests/JsonModelReaderTests.swift
@@ -0,0 +1,65 @@
+//
+//  JsonModelReaderTests.swift
+//  Tutor
+//
+//  Created by Fuzzymeme on 31/05/2017.
+//  Copyright © 2017 Fuzzymeme. All rights reserved.
+//
+
+import XCTest
+@testable import Tutor
+
+class JsonModelReaderTests: XCTestCase {
+    
+    private var reader = JsonModelReader()
+    
+    override func setUp() {
+        super.setUp()
+        reader = JsonModelReader()
+    }
+    
+    func test_retrieveFromJsonFile_withSimpleFormatNoGapHistoryOrWrongsAnswers() {
+        let actual = reader.retrieveFromJsonFile(filename: "foo.json")
+        XCTAssert(sampleKnowledgeEntries() == actual)
+    }
+
+    // MARK: - Helpers
+    private func sampleDictionaryString() -> String {
+        return "{\"0\": \"Alpha\", \"1\":\"Beta\"}"
+    }
+    
+    private func sampleKnowledgeEntries() -> [Int: KnowledgeEntry] {
+        let knowledgeEntry0 = KnowledgeEntry(id: 0, english: "autumn", german: "der Herbst", nextQuestionTime: 1358263105338, gapHistory: [899955507, 8433796674], wrongAnswers: [])
+        let knowledgeEntry1 = KnowledgeEntry(id: 1, english: "century", german: "das Jahrhundert", nextQuestionTime: 1358263108626, gapHistory: [799955507, 2433796674], wrongAnswers: [])
+        let knowledgeEntry2 = KnowledgeEntry(id: 59, english: "because", german: "weil", nextQuestionTime: 1326716025112, gapHistory: [800214164, 2433419412], wrongAnswers: JsonModelReaderTests.sampleWrongAnswer())
+        let knowledgeEntry3 = KnowledgeEntry(id: 60, english: "besides", german: "außerdem", nextQuestionTime: 1326715979544, gapHistory: [800215993, 2433415671], wrongAnswers: [])
+        let knowledgeEntry4 = KnowledgeEntry(id: 61, english: "but", german: "aber, sondern", nextQuestionTime: 1358251974096, gapHistory: [800215916, 2433415455], wrongAnswers: [])
+        
+        return [0: knowledgeEntry0, 1: knowledgeEntry1, 59: knowledgeEntry2, 60: knowledgeEntry3, 61: knowledgeEntry4]
+    }
+    
+    private func sampleEntryString() -> String {
+        return "{\n" +
+                "\"0\": {\n" +
+                    "\"id\": 0,\n" +
+                    "\"english\": \"autumn\",\n" +
+                    "\"german\": \"der Herbst\",\n" +
+                    "\"nextQuestionTime\": 1358263105338,\n" +
+                    "\"gapHistory\": [],\n" +
+                    "\"wrongAnswers\": {}\n" +
+                "\"}"
+    }
+
+    static private func sampleWrongAnswer() -> [WrongAnswer] {
+        return [WrongAnswer(id: 194, when: 1326716015112, count: 1)]
+    }
+    
+    private func sampleWrongAnswerString() -> String {
+        return "{\n" +
+        "\"59\": {\n" +
+            "\"id\": 194,\n" +
+            "\"when\": 1326716015112,\n" +
+            "\"count\": 1\n" +
+        "}"
+    }
+}
