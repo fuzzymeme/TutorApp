e19a318551df93b0328e8c002868084e243d4198
fuzzymeme <fuzzymeme@gmail.com>
2017-06-04 21:48:44 +0100

    First Strategy, based on space repetition and prompting with previous wrong answers

---
+++ Tutor/AugmentedSpacedRepetitionStrategy.swift
@@ -0,0 +1,13 @@
+//
+//  AugmentedSpacedRepetitionStrategy.swift
+//  Tutor
+//
+//  Created by Fuzzymeme on 04/06/2017.
+//  Copyright © 2017 Fuzzymeme. All rights reserved.
+//
+
+import Foundation
+
+class AugmentedSpacedRepetitionStrategy {
+    
+}
